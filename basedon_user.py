89e57f85e456c860a84755c46936cedd017f02d3
A.Safarji <a.m.safarji@gmail.com>
2021-12-06 17:24:38 +0300

    Update basedon_user.py
    NLP

--- basedon_user.py
+++ basedon_user.py
@@ -77,7 +77,7 @@ def get_recommendations(book):
     sortedbooks = sorted(similar_books, key = lambda x:x[1], reverse=True)[1:]
     i = 0
     for book in sortedbooks:
-        st.write( " Title: "+ df1.BookTitle[df1["index"] == book[0]]) + "  |  "  + " author: " + df1.author[df1["index"] == book[0]])
+        st.write(" Title: "+ df1.BookTitle[df1["index"] == book[0]] +"  |  "  + " author: " + df1.author[df1["index"] == book[0]])
       
         i = i+1
         if i>10:
