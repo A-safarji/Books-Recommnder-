50655206601e196b12f450fbf56beb3d432f1b52
A.Safarji <a.m.safarji@gmail.com>
2021-12-06 14:21:31 +0300

    Update basedon_user.py
    NLP

--- basedon_user.py
+++ basedon_user.py
@@ -28,6 +28,8 @@ st.markdown("""
 
 
 st.markdown("""
+إسناد التأليف العربي هو مهمة البحث عن مؤلف المستند. لتحقيق هذا الغرض ، يقارن المرء نص الاستعلام بنموذج المؤلف المرشح ويحدد احتمال نموذج الاستعلام.
+
 	Arabic authorship attribution is the task of finding the author of a document.
 	To achieve this purpose, one compares a query text with a model of the candidate author and determines the likelihood of the model for the query.
 	""")
