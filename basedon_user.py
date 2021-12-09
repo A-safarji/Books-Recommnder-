143e9a66d306cbc7340eacf2e6a7d75c10242aac
A.Safarji <a.m.safarji@gmail.com>
2021-12-09 09:05:33 +0300

    Update basedon_user.py
    إ٬[

--- basedon_user.py
+++ basedon_user.py
@@ -54,12 +54,12 @@ if submit:
 
         summWords = sum2(text, max_length=max_lengthy, min_length=15, num_beams=num_beamer, do_sample=True, early_stopping=True, repetition_penalty=1.5, length_penalty=1.5)
 
-    text2 =summWords[0]
-
-    st.write(text2)
+    text2 =summWords[0]["summary_text"]
+st.write(text2)
 	
 	
 	
+st.text_area(label="Output Data:", value=output, height=350)
 	
 	
 
